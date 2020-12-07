0cb27acca1fdb338cbf6c16f4862a4b675f1fef3
walrus10000 <anon51102@gmail.com>
2020-12-07 18:25:59 +0100

    Add form to post new messages

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,31 @@ const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date(),
+    added: new Date().toUTCString(),
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date(),
+    added: new Date().toUTCString(),
   },
 ];
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.get("/new", (req, res, next) => {
+  res.render("form");
+});
+
+router.post("/new", (req, res, next) => {
+  messages.push({
+    text: req.body.messageText,
+    user: req.body.messageUser,
+    added: new Date().toUTCString(),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
